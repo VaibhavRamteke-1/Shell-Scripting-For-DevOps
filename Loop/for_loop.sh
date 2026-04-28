40c43d900cda42bb7cf585cc0845639d9d615ab0
VaibhavRamteke-1 <vaibhavramteke47@gmail.com>
2026-04-28 10:48:43 +0000

    Add for loop guide with real-world DevOps examples

--- Loop/for_loop.sh
+++ Loop/for_loop.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
+
+##When to use
+#When you know the list or count
+#Useful for multiple servers, services, files
+
+#Restart multiple services
+for service in nginx docker ssh
+do
+  systemctl restart $service
+done
+
+#Check status of multiple services
+for service in nginx docker ssh
+do
+  echo "$service status:"
+  systemctl is-active $service
+done
+
+#Check uptime on multiple servers
+for server in server1 server2 server3
+do
+  ssh ubuntu@$server "uptime"
+done
+
+#Backup multiple directories
+for dir in /etc /var /home
+do
+  tar -czf $dir-backup.tar.gz $dir
+done
 
